af199d93c5b04ca407d56d5ffb4a3e2c826ad8b5
Bento Lima <bento.lima@devgo.org>
2023-07-21 09:45:59 -0100

    Add project files.

---
+++ ElsaWorkflowV3/Program.cs
@@ -0,0 +1,52 @@
+using Elsa.EntityFrameworkCore.Extensions;
+using Elsa.EntityFrameworkCore.Modules.Management;
+using Elsa.Extensions;
+using Elsa.Identity.Features;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddElsa(elsa =>
+{
+    // Configure management feature to use EF Core.
+    elsa.UseWorkflowManagement(management => management.UseEntityFrameworkCore(ef => ef.UseSqlite()));
+
+    // Expose API endpoints.
+    elsa.UseWorkflowsApi();
+
+    // Add services for HTTP activities and workflow middleware.
+    elsa.UseHttp();
+
+    // Configure identity so that we can create a default admin user.
+    elsa.UseIdentity(identity =>
+    {
+        identity.UseAdminUserProvider();
+        identity.TokenOptions = options => options.SigningKey = "secret-token-signing-key";
+    });
+
+    // Use default authentication (JWT + API Key).
+    elsa.UseDefaultAuthentication(auth => auth.UseAdminApiKey());
+});
+
+// Add Razor pages.
+builder.Services.AddRazorPages();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseWorkflowsApi();
+app.UseWorkflows();
+app.MapRazorPages();
+app.Run();
